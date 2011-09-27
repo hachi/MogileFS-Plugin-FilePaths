d9db9409783e7e40150643f838d7b1d9cd49bb59
Daniel Frett <daniel.frett@ccci.org>
2011-09-27 16:04:00 -0400

    use MogileFS objects instead of direct db access

--- lib/MogileFS/Plugin/FilePaths.pm
+++ lib/MogileFS/Plugin/FilePaths.pm
@@ -77,9 +77,8 @@ sub load {
         # see if this file exists already
         my $oldfid = MogileFS::Plugin::FilePaths::get_file_mapping( $args->{dmid}, $parentnodeid, $filename );
         if (defined $oldfid && $oldfid) {
-            my $dbh = Mgd::get_dbh();
-            $dbh->do("DELETE FROM file WHERE fid=?", undef, $oldfid);
-            $dbh->do("REPLACE INTO file_to_delete SET fid=?", undef, $oldfid);
+            my $fid = MogileFS::FID->new($oldfid);
+            $fid->delete;
         }
 
         my $fid = $args->{fid};
